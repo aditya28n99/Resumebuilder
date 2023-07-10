507852fbfac500df027b73aa163a1e44abd4c294
Aditya Sunil Wakale <adityawakale208@gmail.com>
2023-07-10 16:22:16 +0530

    initializing ideas

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./commoncss.css"
+
+function Header(){
+
+    return(
+        <>
+        <div className="topbar header ">
+            <span className="col-lb">Resume</span><span className="col-db">Builder</span>
+        </div>
+        </>
+    )
+}
+export default Header;
